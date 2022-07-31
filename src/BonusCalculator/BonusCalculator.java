10e998d555a959d3b3ab1fd6e3e9f3e6cf3fe792
joew2k <ogwuchejoseph25@gmail.com>
2022-07-31 10:31:59 +0100

    Completed the calculator functionality

--- src/BonusCalculator/BonusCalculator.java
+++ src/BonusCalculator/BonusCalculator.java
@@ -8,15 +8,16 @@ public class BonusCalculator {
 		// Initialize the the known values
 		int salary = 1000;
 		int bonus = 250;
+		int quota = 10;
 				
 		//Get the unknown values
-		System.out.println("Enter the number of item sold");
+		System.out.println("Enter the number of item sold for the week");
 		Scanner scanner = new Scanner(System.in);
 		int itemsold = scanner.nextInt();
 		scanner.close();
 		
 		//Calculate the bonus for bonus earners
-		if (itemsold > 10) {
+		if (itemsold > quota) {
 			salary += bonus;
 		}
 		// output the results
