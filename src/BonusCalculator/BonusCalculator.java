529502c88f30b210716267e3e5830eec9cd837c7
joew2k <ogwuchejoseph25@gmail.com>
2022-07-31 10:29:47 +0100

    Calculator is working fine using if statement to check sales

--- src/BonusCalculator/BonusCalculator.java
+++ src/BonusCalculator/BonusCalculator.java
@@ -1,9 +1,26 @@
 package BonusCalculator;
 
+import java.util.Scanner;
+
 public class BonusCalculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Initialize the the known values
+		int salary = 1000;
+		int bonus = 250;
+				
+		//Get the unknown values
+		System.out.println("Enter the number of item sold");
+		Scanner scanner = new Scanner(System.in);
+		int itemsold = scanner.nextInt();
+		scanner.close();
+		
+		//Calculate the bonus for bonus earners
+		if (itemsold > 10) {
+			salary += bonus;
+		}
+		// output the results
+		System.out.println("Your salary is $" + salary);
 
 	}
 
