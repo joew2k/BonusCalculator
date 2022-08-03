18952e068d502e5b85350d8ed0c2cabb6072b85d
joew2k <ogwuchejoseph25@gmail.com>
2022-08-03 09:40:33 +0100

    Working with switch statement

--- src/BonusCalculator/TestResult.java
+++ src/BonusCalculator/TestResult.java
@@ -3,6 +3,8 @@
  */
 package BonusCalculator;
 
+import java.util.Scanner;
+
 /**
  * @author Covenant Visions
  *
@@ -14,6 +16,33 @@ public class TestResult {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// Get students score.
+		System.out.println("Enter your test score");
+		Scanner scanner = new Scanner(System.in);
+		Double score = scanner.nextDouble();
+		scanner.close();
+		// Determine the numeric grade
+		char grade;
+		if (score< 60) {
+			grade = 'F';
+		}
+		else if(score<70) {
+			grade = 'E';
+		}
+		else if(score<80) {
+			grade = 'D';
+		}
+		else if(score<85) {
+			grade = 'C';
+		}
+		else if(score<90) {
+			grade = 'B';
+		}
+		else grade = 'A';
+		
+		// output grade
+		System.out.println("Your grade is " + grade);
+		
 
 	}
 
